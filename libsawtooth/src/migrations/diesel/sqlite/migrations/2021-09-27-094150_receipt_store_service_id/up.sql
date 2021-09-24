4e29b05370ab0dff16a83a14788c1c4486c9cfff
Isabel Tomb <tomb@bitwise.io>
2021-09-24 12:32:02 -0500

    Modify postgres and sqlite migrations
    Add a migration to modify the `transaction_receipt` tables in
    postgres/migrations and sqlite/migrations to include a new nullable
    column 'service_id' this column is meant to hold an ID that that is
    specific to a `DieselReceiptStore` instance.
    
    Additionally, remove the unique constraint from the idx column. This is
    because indexes only need to be unique for each service id.
    
    Signed-off-by: Isabel Tomb <tomb@bitwise.io>

---
+++ libsawtooth/src/migrations/diesel/sqlite/migrations/2021-09-27-094150_receipt_store_service_id/up.sql
@@ -0,0 +1,26 @@
+--- Copyright 2018-2021 Cargill Incorporated
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- -----------------------------------------------------------------------------
+
+-- Modify idx to drop uniqueness constraint and add a `service_id` column
+CREATE TABLE IF NOT EXISTS transaction_receipt_copy (
+    transaction_id              TEXT PRIMARY KEY,
+    idx                         BIGINT NOT NULL,
+    service_id                  TEXT
+);
+
+INSERT INTO transaction_receipt_copy(transaction_id, idx)
+    SELECT transaction_id, idx FROM transaction_receipt;
+DROP TABLE transaction_receipt;
+ALTER TABLE transaction_receipt_copy RENAME TO transaction_receipt;
