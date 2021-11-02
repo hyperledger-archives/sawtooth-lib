c5a6818175736773c11cc13727a5f373126204a6
Peter Schwarz <pschwarz@bitwise.io>
2021-11-02 08:47:55 -0500

    Remove btree-store
    This change removes the feature "btree-store" as it is no longer
    necessary.  The generic store concept has been replaced with
    domain-specific store traits, such as the receipt::store::ReceiptStore.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libsawtooth/src/store/mod.rs
+++ libsawtooth/src/store/mod.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[cfg(feature = "btree-store")]
-pub mod btree;
 mod error;
 
 use std::convert::TryInto;
